c7f17ce314794f86e650a79578328dc1f20f221c
Nabil El Maaroufi <93434382+nabilelmaa@users.noreply.github.com>
2024-01-09 22:05:00 +0100

    UI enhancement

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { IoArrowForwardCircleOutline } from "react-icons/io5";
+import { CgMoreO } from "react-icons/cg";
 
 function Hero() {
   return (
@@ -20,17 +22,17 @@ function Hero() {
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="flex items-center w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
               href="/get-started"
             >
-              Get Started
+              Get Started <IoArrowForwardCircleOutline className="ml-2" />
             </a>
 
             <a
-              className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+              className="w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto flex items-center"
               href="/about"
             >
-              Learn More
+              Learn More <CgMoreO className="ml-2" />
             </a>
           </div>
         </div>
--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" class="scroll-smooth">
       <body className={inter.className}>
         <Header />
         {children}
